e89f47854fcdbc173990467a39a58f04e3666bf3
Nicolas Diaz <nicolas.diaz@avvy.co>
2022-09-20 12:49:12 -0300

    strict mode off && collection with 15 nfts

--- src/utils/collection.js
+++ src/utils/collection.js
@@ -8,7 +8,12 @@ export const collection = [
   { id: '397', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
   { id: '563', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
   { id: '128', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
-  { id: '98', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '123', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '13', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '11', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '112', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '10', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
+  { id: '12', contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d' },
   {
     id: '30553606573219150352991292921105176340809048341686170040023897701179038105601',
     contract: '0x495f947276749ce646f68ac8c248420045cb7b5e',
